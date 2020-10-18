7d313f26b62f2c1c956139c0cd875f3922f66e0a
Zoltán Péter Szikszó <szikszo.zoltan@gmail.com>
2020-10-19 01:43:31 +0300

    Some changes

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let typewriter = new Typewriter(typing, {
     loop: true
 });
 
-typewriter.typeString('Business Information Technology Student.')
+typewriter.typeString('Business Information Technology Student')
     .pauseFor(2500)
     .deleteAll()
-    .typeString('Studying Software Engineering.')
+    .typeString('Studying Software Engineering')
     .pauseFor(2500)
     .deleteAll()
-    .typeString('Wannabe Full-Stack Developer.')
+    .typeString('Wannabe Front-End Developer')
     .pauseFor(2500)
     .start();
