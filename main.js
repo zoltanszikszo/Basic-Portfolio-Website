0de03972e12a1f537e484bce10b0a698a60f2dfc
zoltanszikszo <szikszo.zoltan@gmail.com>
2021-02-06 19:19:57 +0200

    Rebuilding portfolio without TypeWriting

--- main.js
+++ main.js
@@ -3,15 +3,7 @@
 let typing = document.getElementById('typing');
 
 let typewriter = new Typewriter(typing, {
-    loop: true
+    loop: false
 });
 
-typewriter.typeString('Business Information Technology Student')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Studying Software Engineering')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Wannabe Front-End Developer')
-    .pauseFor(2500)
-    .start();
+typewriter.typeString('Software Engineering Student')
